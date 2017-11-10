055ce04b6af390b459ab9523e5308b9658a41441
sapazos <sapazos@gmail.com>
2017-11-10 12:34:39 -0300

    Arreglo bug en reportes no se exportaban a pdf

--- php/operaciones/reportes/ci_proyectos_por_persona.php
+++ php/operaciones/reportes/ci_proyectos_por_persona.php
@@ -1,39 +1,40 @@
 <?php
 class ci_proyectos_por_persona extends investigaciones_ci
 {
-	//-----------------------------------------------------------------------------------
-	//---- cuadro -----------------------------------------------------------------------
-	//-----------------------------------------------------------------------------------
+    protected $s__filtro;
+    //-----------------------------------------------------------------------------------
+    //---- cuadro -----------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
 
-	function conf__cuadro(investigaciones_ei_cuadro $cuadro)
-	{
-		$where = $this->dep('filtro')->get_sql_where();
-		if ($where == '1=1')
-			return;
-		$datos = toba::consulta_php('co_proyectos')->get_proyectos_por_persona($where); 
-		$cuadro->set_datos($datos);
+    function conf__cuadro(investigaciones_ei_cuadro $cuadro)
+    {
+            $where = $this->dep('filtro')->get_sql_where();
+            if ($where == '1=1')
+                    return;
+            $datos = toba::consulta_php('co_proyectos')->get_proyectos_por_persona($where); 
+            $cuadro->set_datos($datos);
 
-	}
-	
-	//-----------------------------------------------------------------------------------
-	//---- filtro -----------------------------------------------------------------------
-	//-----------------------------------------------------------------------------------
+    }
 
-	function conf__filtro(investigaciones_ei_filtro $filtro)
-	{   
-		if (isset($this->s__filtro)) {
-			$filtro->set_datos($this->s__filtro);
-		}   
-	}  
-	
-	function evt__filtro__filtrar($datos)
-	{   
-		$this->s__filtro = $datos;
-	}   
+    //-----------------------------------------------------------------------------------
+    //---- filtro -----------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
 
-	function evt__filtro__cancelar()
-	{   
-		unset($this->s__filtro);
-	}    
+    function conf__filtro(investigaciones_ei_filtro $filtro)
+    {   
+            if (isset($this->s__filtro)) {
+                    $filtro->set_datos($this->s__filtro);
+            }   
+    }  
+
+    function evt__filtro__filtrar($datos)
+    {   
+            $this->s__filtro = $datos;
+    }   
+
+    function evt__filtro__cancelar()
+    {   
+            unset($this->s__filtro);
+    }    
 }
 ?>
