dd9baab36fe3a3ba3774e9a3920ef8b3c3adaaf8
sapazos <sapazos@gmail.com>
2019-08-01 13:25:48 -0300

    me faltaban los archivos

---
+++ php/operaciones/propuestas/ci_cargar_evaluador_pii.php
@@ -0,0 +1,99 @@
+<?php
+class ci_cargar_evaluador_pii extends investigaciones_ci
+{ 
+    //-------------------------------------------------------------------------
+    function relacion()
+    {
+        return $this->controlador->dep('relacion');
+    }
+
+    //-------------------------------------------------------------------------
+    function tabla($id)
+    {
+        return $this->controlador->dep('relacion')->tabla($id);    
+    }
+
+    //-----------------------------------------------------------------------------------
+    //---- cuadro -----------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
+
+    function conf__cuadro(investigaciones_ei_cuadro $cuadro)
+    {
+        $where = $this->dep('filtro')->get_sql_where();
+        $aux = array();
+        $where = $where . ' AND propuestas.estado = 1 AND propuestas.tipo = 2';
+        $datos = toba::consulta_php('co_propuestas')->get_propuestas($where);
+        foreach ($datos as $dat) {
+            $nombre = toba::consulta_php('co_personas')->get_datos_persona($dat['proponente']);
+            $dat['nombre_completo'] = $nombre['nombre_completo']; 
+            if (isset($dat['asignatura'])) {
+                $nombre = toba::consulta_php('co_carreras')->get_asignaturas('actividad = '.$dat['asignatura']);
+                $dat['asignatura_desc'] = $nombre[0]['descripcion'];     
+            } 
+            if (isset($dat['ambito'])) {
+                $nombre = toba::consulta_php('co_carreras')->get_ambitos('ambito = '.$dat['ambito']);
+                $dat['ambito_desc'] = $nombre[0]['descripcion'];     
+            }             
+            $aux[] = $dat;
+        }
+        //$datos_ordenados = rs_ordenar_por_columna($aux, 'numero');
+        $cuadro->set_datos($aux);
+    }
+
+    function evt__cuadro__seleccion($seleccion)
+    {
+        $this->relacion()->cargar($seleccion);
+        $this->set_pantalla('edicion');
+    }    
+
+
+    function conf__form(investigaciones_ei_formulario $form)
+    {
+        if ($this->relacion()->esta_cargada()) {
+            $datos = $this->tabla('propuestas')->get();
+            $form->set_datos($datos);
+        }
+    }        
+
+    function evt__form__modificacion($datos)
+    {
+        $datos['estado'] = 2; // pasa a estado en evaluacion
+        $this->tabla('propuestas')->set($datos);
+    } 	
+    //-----------------------------------------------------------------------------------
+    //---- filtro -----------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
+
+    function conf__filtro(investigaciones_ei_filtro $filtro)
+    {
+        if (isset($this->s__filtro)) {
+            $filtro->set_datos($this->s__filtro);
+        }
+    }
+
+    function evt__filtro__filtrar($datos)
+    {
+        $this->s__filtro = $datos;
+    }
+
+    function evt__filtro__cancelar()
+    {
+        unset($this->s__filtro);
+}    
+    //-----------------------------------------------------------------------------------
+    //---- Eventos ----------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
+
+    function evt__procesar()
+    {
+        $this->dep('relacion')->sincronizar();
+        $this->dep('relacion')->resetear();
+        $this->set_pantalla('seleccion');
+    }
+
+    function evt__cancelar()
+    {
+        $this->dep('relacion')->resetear();
+        $this->set_pantalla('seleccion');
+    }
+}
