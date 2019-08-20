1c7bd2620bd8e78fcd5a270fa3cd44ade5a55b0a
sapazos <sapazos@gmail.com>
2019-08-20 10:41:28 -0300

    cambio a co_personas en combo evaluador modificacion

--- metadatos/componentes/toba_ei_formulario/dump_280000919.sql
+++ metadatos/componentes/toba_ei_formulario/dump_280000919.sql
@@ -954,11 +954,11 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	NULL, --estado_defecto
 	'1', --solo_lectura
 	'0', --solo_lectura_modificacion
-	'get_evaluadores', --carga_metodo
+	'get_personas', --carga_metodo
 	NULL, --carga_clase
 	NULL, --carga_include
 	NULL, --carga_dt
-	'280000025', --carga_consulta_php
+	'280000012', --carga_consulta_php
 	NULL, --carga_sql
 	'investigaciones', --carga_fuente
 	NULL, --carga_lista
